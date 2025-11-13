c99833ccd1cf5db7be6354ffc45a3e00b218f367
homeservicespecialistas-cmyk <homeservicespecialistas@gmail.com>
2025-11-13 11:40:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 const swiper = new Swiper('.mySwiper', {
   effect: 'flip',
   grabCursor: true,
-  speed: 700,
+  speed: 900,
   flipEffect: {
     slideShadows: true,
     limitRotation: true,
   },
+  allowTouchMove: true, // puedes girar con el dedo
 });
 
-// Botones manuales
+// Botones
 document.getElementById('next').addEventListener('click', () => swiper.slideNext());
 document.getElementById('prev').addEventListener('click', () => swiper.slidePrev());
